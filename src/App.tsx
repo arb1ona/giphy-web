051f6262dc56f54aa3501cad51a571001185f5f4
Arbiona <arbiona.shahu@gmail.com>
2024-10-06 19:33:34 +0200

    add some `styling`

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import "./App.css";
 import SearchForm from "./components/SearchForm";
 import ImageGallery from "./components/ImageGallery";
-import Pagination from "./components/Pagination";
 import { GiphyProvider } from "./contexts/GiphyContext";
 
 function App() {
   return (
     <GiphyProvider>
       <div className="App">
-        <h1>Giphy Image Explorer</h1>
+        <h1 className="heading">Giphy Image Explorer</h1>
         <SearchForm />
         <ImageGallery />
-        {/* <Pagination /> */}
       </div>
     </GiphyProvider>
   );
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,7 @@
 import React, { type ChangeEvent, type FormEvent } from "react";
 import { useGiphyContext } from "../contexts/GiphyContext";
 import { useGiphySearch } from "../hooks/useGiphySearch";
+import styles from "./SearchForm.module.css";
 
 const SearchForm = () => {
   const {
@@ -19,29 +20,34 @@ const SearchForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search for images"
+        className={styles.input}
       />
       <input
         type="text"
         value={overlayText}
         onChange={(e) => setOverlayText(e.target.value)}
         placeholder="Text to display on images"
+        className={styles.input}
       />
 
       <select
         value={textPosition}
         onChange={(e) => setTextPosition(e.target.value)}
+        className={styles.select}
       >
         <option value="top">on top of image - center top</option>
         <option value="bottom">on top of image - center bottom</option>
         <option value="below">below image - center</option>
       </select>
-      <button type="submit">Search</button>
+      <button type="submit" className={styles.button}>
+        Search
+      </button>
     </form>
   );
 };
